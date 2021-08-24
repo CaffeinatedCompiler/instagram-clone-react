3ceabec5aed2541b7c62b89067358f339b4ca02d
Kenway <kenway.chung@protonmail.com>
2021-08-24 19:29:00 -0400

    Add type for HomeBox children

--- src/components/HomeBox/HomeBox.tsx
+++ src/components/HomeBox/HomeBox.tsx
@@ -1,6 +1,7 @@
+import { ReactElement } from 'react';
 import Navbar from '../Navbar/Navbar';
 import './HomeBox.scss';
-export default function HomeBox({ children }) {
+export default function HomeBox({ children }: { children: ReactElement }) {
   return (
     <div className="home">
       <Navbar />
